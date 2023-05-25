bb7532d994e8f7710984438e2dfff8632429d11b
Leksandra87 <petroselinuma@gmail.com>
2023-05-25 21:15:45 +0300

    done

--- Занятие 1. Основы ООП/1. Практические задания/task1_Glass__init__/main.py
+++ Занятие 1. Основы ООП/1. Практические задания/task1_Glass__init__/main.py
@@ -3,10 +3,25 @@ from typing import Union
 
 class Glass:
     def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
-        #  TODO инициализировать объект "Стакан"
+        if not isinstance(capacity_volume, (int, float)):
+            raise TypeError
+        if not isinstance(occupied_volume, (int, float)):
+            raise TypeError
+        if capacity_volume <= 0:
+            raise ValueError
+        if occupied_volume > capacity_volume:
+            raise ValueError('Объм вмещаемой жидкости не может быть больше объема стакана')
+        self.capacity_volume = capacity_volume
+        self.occupied_volume = occupied_volume
+        #   инициализировать объект "Стакан"
 
 
 if __name__ == "__main__":
-    ...  # TODO инициализировать два объекта типа Glass
+    glass_1 = Glass(200, 100)
+    glass_2 = Glass(500, 50)
+    print(glass_1.occupied_volume)
+    print(glass_2.capacity_volume)
 
-    # TODO попробовать инициализировать не корректные объекты
+    ...  # инициализировать два объекта типа Glass
+
+    #  попробовать инициализировать не корректные объекты
--- Занятие 1. Основы ООП/1. Практические задания/task6_Node_methods/main.py
+++ Занятие 1. Основы ООП/1. Практические задания/task6_Node_methods/main.py
@@ -10,18 +10,27 @@ class Node:
         :param value: Любое значение, которое помещено в узел
         :param next_: следующий узел, если он есть
         """
-        ...  # TODO добавить атрибуты
+        self.value = value
+        self.next = next_
+        ...  #  добавить атрибуты
 
     def get_value(self) -> Any:
         """Метод, который возвращает значение атрибута value"""
-        ...  # TODO вернуть значение узла
+        return self.value
+        ...  #  вернуть значение узла
+    def get_next(self) -> Optional["Node"]:
+        return self.next
 
-    # TODO добавить метод get_next
+    #  добавить метод get_next
 
 
 if __name__ == '__main__':
     first_node = Node(1)  # первый узел
     second_node = Node(2)  # второй узел
+    third_node = Node(3)
+    print(first_node.get_value(), first_node.get_next())
+    print(second_node.get_value(), second_node.get_next())
+    print(third_node.get_value(), third_node.get_next())
 
-    # TODO с помощью метода распечатать значение первого узла
-    # TODO  с помощью метода распечатать следующий узел второго узла
+    #  с помощью метода распечатать значение первого узла
+    #  с помощью метода распечатать следующий узел второго узла
